ab506dc9c6c97f8ec0f00c657700b3e01a3e7d29
theMarineDataScientist <jorgemfa@gmail.com>
2020-03-05 16:04:01 +0000

    Update Dependences.R

--- Dependences.R
+++ Dependences.R
@@ -46,6 +46,22 @@ for(package in packages.to.use) {
 ## Functions
 
 
+distinctColors <- function(n) {
+  
+  library(RColorBrewer)
+  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector <- unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  col_vector_long <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+  
+  if(n > 74) { col_vector <- sample(col_vector_long, n,replace=FALSE) }
+  if(n <= 74) { col_vector <- sample(col_vector, n,replace=FALSE) }
+
+  return(col_vector)
+  
+}
+
+## --------------------
+
 getLocation <- function(coordLon,coordLat) {
   
   construct.geocode.url <- function(coordLon,coordLat, return.call = "json", sensor = "false") {
