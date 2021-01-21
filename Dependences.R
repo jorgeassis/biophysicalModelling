d73e221cd54e7fd9a2018b99b95df182a90dcf87
theMarineDataScientist <jorgemfa@gmail.com>
2021-01-21 18:49:31 +0000

    Update Dependences.R

--- Dependences.R
+++ Dependences.R
@@ -135,7 +135,7 @@ distinctColors <- function(n) {
   
   col_vector <- 0
   errorManagment <- 0
-  while(length(col_vector) != n) {
+  while(length(unique(col_vector)) != n) {
     
     qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
     col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
